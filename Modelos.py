70f6af23d29326fa01fada655b73f11fa80e45d3
Estebin21 <esteban.narvaez.acurio@udla.edu.ec>
2025-05-27 18:13:17 -0500

    catequistas crud a medias

---
+++ Modelos.py
@@ -0,0 +1,8 @@
+# from flask import Flask, render_template, request, redirect, url_for
+# from flask_sqlalchemy import SQLAlchemy
+# import urllib
+# import pyodbc
+# import app
+# class Catequizando(app.db.Model):
+#     __tablename__='Catequizando'
+#     id=app.db.Column(app.db.Integer, primary_key=True)
